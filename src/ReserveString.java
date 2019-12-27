c6f1c3c15b48d5fc966528909c0a5bb0abbd78d0
devxinstructor <devx@gmail.com>
2019-12-26 20:58:56 -0600

    reverse string

--- src/ReserveString.java
+++ src/ReserveString.java
@@ -1,5 +1,9 @@
 public class ReserveString {
     public static void main(String[] args) {
+        StringBuilder s = new StringBuilder();
+        s.append("Hello world");
+        System.out.println(s);
+        System.out.println(s.reverse());
 
     }
 }
