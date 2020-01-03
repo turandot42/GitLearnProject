23dbb8e2bde40d60939c8965981b90f9aee08b92
aainura <57549747+aainura@users.noreply.github.com>
2020-01-02 20:47:17 -0500

    Update man.java
    Subtraction added

--- src/man.java
+++ src/man.java
@@ -4,6 +4,8 @@ public static void main(String[]args){
     int a=5;
     int b=2
         int sum=a+b;
+    sub = a-b;
     System.out.println(sum);
+    System.out.println(sub);
         }
         } . 
